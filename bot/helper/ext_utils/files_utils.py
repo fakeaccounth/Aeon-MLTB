f81decb3207a5d385b007f4b8bffd2e0eec3a06c
J͙a͙c͙k͙ S͙p͙a͙r͙r͙o͙w͙ <138965244+Lord-JackSparrow@users.noreply.github.com>
2025-01-03 18:24:07 +0530

    .

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -561,7 +561,7 @@ async def get_ss(up_path, ss_no):
     thumbs_path, tstamps = await take_ss(up_path, total=ss_no, gen_ss=True)
     th_html = f"<h4>{ospath.basename(up_path)}</h4><br><b>Total Screenshots:</b> {ss_no}<br><br>"
     th_html += "".join(
-        f'<img src="{upload_image(ospath.join(thumbs_path, thumb))}"><br><pre>Screenshot at {tstamps[thumb]}</pre>'
+        f'<img src="https://graph.org{upload_file(ospath.join(thumbs_path, thumb))[0]}"><br><pre>Screenshot at {tstamps[thumb]}</pre>'
         for thumb in natsorted(await listdir(thumbs_path))
     )
     await aiormtree(thumbs_path)
